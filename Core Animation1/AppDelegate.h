322192e1a11c96844f7d618618f2f414cc69662b
qiqiqimu <heyafei@12138@163.com>
2016-12-09 15:34:40 +0800

    Initial Commit

---
+++ Core Animation1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Core Animation1
+//
+//  Created by apple on 16/12/9.
+//  Copyright © 2016年 贺亚飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Core Animation1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Core Animation1
+//
+//  Created by apple on 16/12/9.
+//  Copyright © 2016年 贺亚飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
